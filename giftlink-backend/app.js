afc742edc72bfb95aa24a58cdcddf34903298eed
rodrigo gayoso medina <rodrigogm.dev@gmail.com>
2025-08-13 16:24:15 -0400

    backend routes issues

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,6 +1,5 @@
 /*jshint esversion: 8 */
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
@@ -8,38 +7,31 @@ const pinoLogger = require('./logger');
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
-const pinoHttp = require('pino-http');
-const logger = require('./logger');
-
-// Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-const giftRoutes = require('./routes/giftRoutes');
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-const searchRoutes = require('./routes/searchRoutes');
-
 
 const app = express();
+app.use("*",cors());
 const port = 3060;
 
-
-app.use("*",cors());
-app.use(express.json());
-app.use(pinoHttp({ logger }));
-
-// Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-app.use('/api/gifts', giftRoutes);
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use('/api/search', searchRoutes)
-
 // Connect to MongoDB; we just do this one time
-
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
     .catch((e) => console.error('Failed to connect to DB', e));
+
+
+app.use(express.json());
+
+// Route files
+const giftRoutes = require('./routes/giftRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const pinoHttp = require('pino-http');
+const logger = require('./logger');
+
+app.use(pinoHttp({ logger }));
+
+// Use Routes
+app.use('/api/gifts', giftRoutes);
+app.use('/api/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
@@ -54,4 +46,3 @@ app.get("/",(req,res)=>{
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-    
