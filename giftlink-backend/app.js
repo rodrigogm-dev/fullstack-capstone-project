eebea290092634ddd843f7ffa69ce6ed43c6f2ee
rodrigo gayoso medina <rodrigogayoso@gmail.com>
2025-08-18 19:16:44 -0400

    proxy issue solved

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const pinoLogger = require('./logger');
+// const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
@@ -14,7 +14,7 @@ const port = 3060;
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
+    // pinoLogger.info('Connected to DB');
 })
     .catch((e) => console.error('Failed to connect to DB', e));
 
@@ -24,10 +24,10 @@ app.use(express.json());
 // Route files
 const giftRoutes = require('./routes/giftRoutes');
 const searchRoutes = require('./routes/searchRoutes');
-const pinoHttp = require('pino-http');
+// const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
-app.use(pinoHttp({ logger }));
+// app.use(pinoHttp({ logger }));
 
 // Use Routes
 app.use('/api/gifts', giftRoutes);
--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
-const logger = require('../logger');
+// const logger = require('../logger');
 
 // Get all gifts
 router.get('/', async (req, res, next) => {
-    logger.info('/ called');
+    // logger.info('/ called');
     try {
         const db = await connectToDatabase();
 
@@ -13,7 +13,7 @@ router.get('/', async (req, res, next) => {
         const gifts = await collection.find({}).toArray();
         res.json(gifts);
     } catch (e) {
-        logger.console.error('oops something went wrong', e)
+        // logger.console.error('oops something went wrong', e)
         next(e);
     }
 });
