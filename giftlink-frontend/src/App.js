c9b0b9c139bc3cf5becb4c7d77224bde30c8e8f5
Rodrigo Gayoso Medina <rodrigogm.dev@gmail.com>
2025-08-19 23:49:47 -0400

    ProfilePage

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
+import Profile from './components/Profile/Profile';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import SearchPage from './components/SearchPage/SearchPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/app/profile" element={<Profile/>} />
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/search" element={<SearchPage/>} />
